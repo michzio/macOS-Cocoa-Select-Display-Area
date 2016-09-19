7f975b684fd867d9a821bb069d52d8d3c2d27075
michzio <michzio@hotmail.com>
2016-09-19 16:18:22 +0200

    Initial Commit

---
+++ SelectionView/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SelectionView
+//
+//  Created by Michal Ziobro on 19/09/2016.
+//  Copyright © 2016 Michal Ziobro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
